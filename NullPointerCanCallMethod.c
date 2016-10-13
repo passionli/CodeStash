bf9404d2d0e5567a5ba46476907aa69a9d26cbec
GuangLi <passionli@vip.qq.com>
2016-10-14 00:49:12 +0800

    Update NullPointerCanCallMethod.c

--- NullPointerCanCallMethod.c
+++ NullPointerCanCallMethod.c
@@ -1,7 +1,8 @@
+
 class A{
-	int i;
 public:
-	int Foo(){
+	int i;
+	virtual int Foo(){
 		return (int)this;
 	}
 
@@ -9,10 +10,17 @@ public:
 		//segment fault for this pointer
 		return this->i;
 	}
+
+	int GetOffset(){
+		//this is ok.For not reading the value of the address
+		return (int)&(this->i);
+	}
 };
 
 int main(int argc, _TCHAR* argv[])
 {
+	int address = 0x1000;
+	int offset = ((A*)address)->GetOffset();
 	//this == 0
 	int addr = ((A *)0x0)->Foo();
 	//this == 0x1000
